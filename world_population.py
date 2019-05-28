ed8b1e080fa7a8e10fdfa2e13a385bb0d8ae728c
Maria Mileeva <mileeva.mary@gmail.com>
2019-05-28 21:00:39 +0300

    Building a World Map

--- world_population.py
+++ world_population.py
@@ -1,14 +1,37 @@
 import json
+import pygal
+
+from country_codes import get_country_code
+from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
 
 # Load the data into a list
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-# Print the 2010 population for each country
+# Build a dictionary of population data
+cc_population = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict["Country Name"]
         population = int(float(pop_dict["Value"]))
-        print(country_name + ': ' + str(population))
+        code = get_country_code(country_name)
+        if code:
+            cc_population[code] = population
 
+# Group the countries into 3 populattion levels
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_population.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+wm_style = RS('#336699', base_style=LCS)
+wm = pygal.maps.world.World(style=wm_style)
+wm.title = 'World Population in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1b', cc_pops_2)
+wm.add('>1b', cc_pops_3)
