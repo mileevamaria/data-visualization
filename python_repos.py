44f1bef4cc6dab2b282f566124851653a2e50ef6
Maria Mileeva <mileeva.mary@gmail.com>
2019-05-29 20:15:40 +0300

    Adding custom Tooltips

--- python_repos.py
+++ python_repos.py
@@ -19,16 +19,27 @@ print("Repositories returned:", len(repo_dicts))
 # Examine the first repository
 repo_dict = repo_dicts[0]
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    # Get the project description, if one is available
+    description = repo_dict['description']
+    if not description:
+        description = 'No description provided'
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': description,
+        'x-link': repo_dict['html_url'],
+        }
+    plot_dicts.append(plot_dict)
 
 # Make visualization
 my_style = LS('#333366', base_style=LCS)
 my_style.title_font_size = 24
 my_style.label_font_size = 14
-my_style.major_label_font_size = 18
+my_style.major_label_font_size = 14
 
 my_config = pygal.Config()
 my_config.x_label_rotation = 45
@@ -41,5 +52,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
